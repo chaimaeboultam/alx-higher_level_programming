8e2bfd5e9e3cd7e321042514eb5ffdd2c4ebe96e
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-10-31 02:47:59 -0700

    .

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+my_str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] +  str[107:112] + str[:6]
-print(str)
+my_str = my_str[39:67] + my_str[107:112] + my_str[:6]
+print(my_str)
