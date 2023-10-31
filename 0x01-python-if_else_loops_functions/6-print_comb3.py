1394abacaefa743b6492e8592f7199468846665b
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-10-31 09:48:35 -0700

    .

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,6 @@
 
 for i in range(9):
     for j in range(10):
-        print(f"{i}{j}", end='\n' if i == 8 else ", ")
+        if i<j:
+            print("{:d}{:d}".format(i,j), end='\n' if i == 8 and i==9 else ", ")
 
