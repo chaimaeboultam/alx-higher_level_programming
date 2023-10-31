410c0750f21557e9256060dbd42c4c9ed70d5118
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-10-31 10:58:53 -0700

    .

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+def islower(c):
+    return ord('a') <= ord(c) <= ord('z')
+
 def uppercase(s):
     for c in s:
         print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32), end="")
+    print("")
