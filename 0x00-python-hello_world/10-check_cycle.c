0c196e610d63f0237da58a302e2a7692bef9116d
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-01 13:57:01 -0700

    .

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,13 +13,13 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list->next;
 
-	while (fast != NULL && fast->next != NULL && slow != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
+		slow = slow->next;
+                fast = fast->next->next;
+
 		if (slow == fast)
 			return (1);
-
-		slow = slow->next;
-		fast = fast->next->next;
 	}
 	return (0);
 }
