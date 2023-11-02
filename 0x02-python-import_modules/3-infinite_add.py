71186e384eb9c5552559a7be04ab527b7f07831f
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-02 03:46:44 -0700

    .

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     s = 0
-    for i in range(len(sys.argv)-1):
-        s += int(sys.argv[i+1])
+    for i in range(1, len(sys.argv)):
+        s += int(sys.argv[i])
     print(s)
