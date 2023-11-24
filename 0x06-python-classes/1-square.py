77d34bbe02cdee564166fdd329748bdd76d04f07
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-24 07:59:54 -0800

    .

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module with a Square class"""
+
+
+class Square:
+    """
+    A representation of a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
