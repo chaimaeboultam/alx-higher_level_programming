e33cf960a7b622ae4f05cdc04ec1f5a2f56b9919
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-24 07:56:30 -0800

    .

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""Module with a simple Square class"""
+
+
 class Square:
+    """
+    A simple representation of a square.
+
+    Attributes:
+        No attributes at the moment.
+    """
     pass
