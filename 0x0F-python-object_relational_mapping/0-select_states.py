c2be373c045d947d14ee1ebd53f5d54710bf42e2
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-02-19 07:52:22 -0800

    .

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
-import MySqldb
-from sys imort argv
+""" Script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
 
-#the code should not be executed when it imported
+# The code should not be executed when imported
 if __name__ == '__main__':
 
-    #make a connection with MySql server
-    db = MySqldb.connect(host = "localhost", port = 3306, user = argv[1],
-                         passrd = argv[2], db = argv[3])
+    # Make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-     # It gives us the ability to have multiple seperate working environments
-     # through the same connection to the database.
-     cur = db.cursor()
-     cur.execute("SELECT *FROM states OREDERED BY id")
+    # It gives us the ability to have multiple separate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
 
-     rows = cur.fetchall()
-     for i in rows:
-         print(i)
+    # Execute the SQL query to select all states and order them by id
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-     #clean up process
-     cur.close()
-     db.close()
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Clean up process
+    cur.close()
+    db.close()
+
