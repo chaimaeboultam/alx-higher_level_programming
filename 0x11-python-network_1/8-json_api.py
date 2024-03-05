a7c8e5eef516614c1b091edcd81eb151b302fecb
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-03-05 15:45:36 -0800

    .

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+takes in a string and sends a search request to the Star Wars API
+"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
+    """
+    takes in a string and sends a search request to the Star Wars API
+    """
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = argv[1]
     else:
         q = ""
 
     payload = {'q': q}
-    url = 'http://0.0.0.0:5000/search_user'
-    response = requests.post(url, data=payload)
 
+    r = requests.post(url, data=payload)
     try:
-        json_response = response.json()
+        json_response = r.json()
         if json_response:
             print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
         else:
