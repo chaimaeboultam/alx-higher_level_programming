316071fa830aeb89a076926097a516705bfc14b9
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-03-05 03:22:52 -0800

    .

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,17 +2,17 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
     
-    start = 0
-    end = len(list_of_integers) - 1
+    left = 0
+    right = len(list_of_integers) - 1
     
-    while start < end:
-        mid = (start + end) // 2
+    while left < right:
+        mid = (left + right) // 2
         
         if list_of_integers[mid] < list_of_integers[mid + 1]:
-            start = mid + 1
+            left = mid + 1
         else:
-            end = mid
+            right = mid
     
-    return list_of_integers[start]
+    return list_of_integers[left]
 
 
