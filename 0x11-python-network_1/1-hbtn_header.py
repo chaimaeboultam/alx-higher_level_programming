4d6b2c545f30aab7c8dbdd89b60f5ec5eeaa45c9
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-03-05 06:06:29 -0800

    .

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,5 @@ import sys
 if  __name__ == "__main__":
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as respnse:
+    with urllib.request.urlopen(url) as response:
         print(response.headers.get('X-Request-Id'))
