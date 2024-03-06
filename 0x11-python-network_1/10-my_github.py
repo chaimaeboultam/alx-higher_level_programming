b8e535ef36421006b6f029eb6bec5dbe536b61a9
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-03-06 09:53:04 -0800

    .

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     """
     username = argv[1]
     password = argv[2]
-    url = 'https://api.github.com/chaimaeboultam'
+    url = 'https://api.github.com/user'
     r = requests.get(url, auth=(username, password))
     try:
         print(r.json().get('id'))
